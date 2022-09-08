9086ac66a500c93a7c6a844c677d4cb2b0400f36
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-09-08 18:03:09 -0300

    ex1feito

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num = Number(prompt(`Digite um número`));
+//const num = 4;
 
+console.log(num);
+
+if (num % 2 === 0) {
+  console.log("por 2.");
+  if (num % 3 === 0) {
+    console.log(" Divisível por 2 e 3.");
+  }
+} else if (num % 3 === 0) {
+  console.log("Só por 3");
+} else {
+  console.log("Burra");
+}
+
+if (num % 2 === 0 || num % 2 === 0) {
+  console.log("Divisível por 2 ou 3.");
+}
