0a4dc94dfe37c9ccdb6fe9a1e14e7e23ecb305dd
Ana Marmitt <anap.mmarmitt@gmail.com>
2022-09-08 18:05:06 -0300

    ex3-feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,23 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+//if(nome === "José"){
+//	console.log("Oi, Zé!");
+//} else {
+//	console.log("Olá, "+nome);
+//}
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+const nome = prompt("Qual seu nome?");
+
+nome === "José" ? console.log("Oi,Zé!") : console.log(`Olá ${nome}!`);
+
+const idade = Number(prompt("Qual a sua idade?"));
+
+idade >= 18
+  ? console.log("Pode tirar a carteira de motorista.")
+  : console.log("Ainda não pode tirar a carteira de motorista.");
+
+//if (idade >= 18) {
+// console.log("pode tirar carteira de motorista!");
+//} else {
+// console.log("Ainda não pode tirar carteira de motorista");
+//}
